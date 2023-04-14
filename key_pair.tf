674cbe2162d6803f3a31f499ca61ea63658d3bcd
ikechukwu <ikechukwu@deriv.com>
2023-04-14 23:46:21 +0800

    wip

--- key_pair.tf
+++
@@ -1,6 +0,0 @@
-#Create key-pair for logging into EC2 in us-east-1
-resource "aws_key_pair" "master-key" {
-  # provider   = aws.region-main
-  key_name   = "jenkins"
-  public_key = file("~/.ssh/jenkins_id_rsa.pub")
-}
