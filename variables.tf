8c105055a89a6a282e8801cd48a3eaffb080fa28
ikechukwu <ikechukwu@deriv.com>
2023-04-14 20:43:38 +0800

    first commit

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "profile" {
+  type    = string
+  default = "default"
+
+}
+
+variable "region-main" {
+  type    = string
+  default = "us-east-1"
+
+}
+
+variable "external_ip" {
+  type    = string
+  default = "0.0.0.0/0"
+
+}
+
+variable "instance-type" {
+  type    = string
+  default = "t3.medium"
+
+}
+
+variable "webserver-port" {
+  type    = number
+  default = 80
+
+}
