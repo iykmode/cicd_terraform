debb3568a4fe660e839e16f9d0f3deda0d098ced
ikechukwu <ikechukwu@deriv.com>
2023-04-15 09:41:30 +0800

    wip

--- webserver-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install NGINX
-sudo yum install nginx -y
-sudo systemctl enable nginx --now
-
-# Install Terraform
-sudo firewall-cmd --zone=public --permanent --add-service=http
-sudo firewall-cmd --reload
